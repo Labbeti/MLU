7fca81a7696e1294c4c2c377568a624197c2bd9c
Labbeti <etienne.labbe31@gmail.com>
2021-01-13 21:25:22 +0100

    Del: Remove old metrics files.

--- mlu/metrics/spider.py
+++
@@ -1,7 +0,0 @@
-
-from mlu.metrics.base import Metric, T_Input, T_Target, T_Output
-
-
-class SPIDER(Metric):
-	def compute_score(self, input_: T_Input, target: T_Target) -> T_Output:
-		raise NotImplementedError
