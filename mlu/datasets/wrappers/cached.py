e635916c5f635704998b40c23648c70c140c1681
Labbeti <etienne.labbe31@gmail.com>
2021-02-10 17:39:42 +0100

    Add: Cached dataset wrapper.

---
+++ mlu/datasets/wrappers/cached.py
@@ -0,0 +1,22 @@
+
+from mlu.datasets.wrappers.base import DatasetWrapper
+from torch.utils.data.dataset import Dataset
+from typing import TypeVar
+
+
+T = TypeVar("T")
+
+
+class CachedDataset(DatasetWrapper):
+	def __init__(self, dataset: Dataset[T]):
+		super().__init__(dataset)
+		self._cached_items = {}
+
+	def __getitem__(self, *args, **kwargs) -> T:
+		args_dict = locals()
+		if args_dict in self._cached_items.keys():
+			item = self._cached_items[args_dict]
+		else:
+			item = super()._dataset[args_dict]
+			self._cached_items[args_dict] = item
+		return item
