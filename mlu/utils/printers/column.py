c2942cae5c89e8112af111c2ee655a03e87cb6c4
Labbeti <etienne.labbe31@gmail.com>
2021-03-01 18:31:33 +0100

    Mod: Update printers with number of iterations instead of progression percent.

--- mlu/utils/printers/column.py
+++ mlu/utils/printers/column.py
@@ -7,24 +7,35 @@ from typing import Dict, List
 class ColumnPrinter(PrinterABC):
 	KEY_MAX_LENGTH = 10
 
-	def __init__(self, print_exec_time: bool = True, double_new_line_at_end: bool = False):
+	def __init__(
+		self,
+		print_epoch: bool = True,
+		print_it: bool = True,
+		print_progression_percent: bool = False,
+		print_exec_time: bool = True,
+	):
 		"""
 			Class for print current values of a training or validation by columns.
 
 			Ex:
 
 			>>> printer = ColumnPrinter()
-			>>> printer.print_current_values({"train/accuracy": 0.89, "train/loss": 1.525}, 33, 100, 2, "train")
+			>>> printer.print_current_values({"train/accuracy": 0.89, "train/loss": 1.525}, 0, 100, 2, "train")
 			... '-      train       -  accuracy  -    loss    -  took (s)  -'
-			... '- Epoch   2 -  33% - 8.9000e-01 - 1.5250e-00 -    0.00    -'
+			... '- Epoch   2 -   0% - 8.9000e-01 - 1.5250e-00 -    0.00    -'
 
+			:param print_epoch: TODO
+			:param print_it: TODO
+			:param print_progression_percent: TODO
 			:param print_exec_time: Print time elapsed with the beginning of the loop (iteration == 0).
 		"""
+		self.print_epoch = print_epoch
+		self.print_it = print_it
+		self.print_progression_percent = print_progression_percent
 		self.print_exec_time = print_exec_time
-		self.double_new_line_at_end = double_new_line_at_end
 
-		self._epoch_start_date = time()
-		self._keys = []
+		self._epoch_start_time = time()
+		self._all_keys = []
 
 	def print_current_values(
 		self,
@@ -39,40 +50,66 @@ class ColumnPrinter(PrinterABC):
 			if name is None:
 				name = "/".join(keys[0].split("/")[:-1]) if len(keys) > 0 else ""
 			keys_names = [key.split("/")[-1] for key in keys]
-			self._keys = keys
-			self._print_header(name, keys_names)
-			self._epoch_start_date = time()
+			self._all_keys = keys
+			self._print_header(name, keys_names, nb_iterations)
+			self._epoch_start_time = time()
 		else:
-			self._keys += list(set(current_values.keys()).difference(self._keys))
+			self._all_keys += list(set(current_values.keys()).difference(self._all_keys))
 
-		progression = int(100 * (iteration + 1) / nb_iterations)
-		content = \
-			["Epoch {:3d} - {:3d}%".format(epoch + 1, progression)] + \
-			[
-				"{:.4e}".format(current_values[key]).center(self.KEY_MAX_LENGTH)
-				if key in current_values.keys() else
-				" " * self.KEY_MAX_LENGTH
-				for key in self._keys
-			]
+		content = []
+		if self.print_epoch:
+			content.append("Epoch {:3d}".format(epoch))
+
+		if self.print_it:
+			it_format = f"{{:{len(str(nb_iterations))}d}}"
+			it_frac_format = f"It {it_format}/{it_format}"
+			content.append(it_frac_format.format(iteration, nb_iterations))
+
+		if self.print_progression_percent:
+			progression = int(100 * (iteration + 1) / nb_iterations)
+			content.append("{:3d}%".format(progression))
+
+		content += [
+			"{:.4e}".format(current_values[key]).center(self.KEY_MAX_LENGTH)
+			if key in current_values.keys() else
+			" " * self.KEY_MAX_LENGTH
+			for key in self._all_keys
+		]
 
 		if self.print_exec_time:
-			content += ["{:.2f}".format(time() - self._epoch_start_date).center(self.KEY_MAX_LENGTH)]
+			content.append("{:.2f}".format(time() - self._epoch_start_time).center(self.KEY_MAX_LENGTH))
 
 		print("- {:s} -".format(" - ".join(content)), end="\r")
 
 		if iteration == nb_iterations - 1:
 			print()
-			if self.double_new_line_at_end:
-				print()
 
-	def _print_header(self, name: str, keys: List[str]):
+	def _print_header(self, name: str, keys: List[str], nb_iterations: int):
 		def filter_name(key_name: str) -> str:
 			if len(key_name) <= self.KEY_MAX_LENGTH:
 				return key_name.center(self.KEY_MAX_LENGTH)
 			else:
 				return key_name[:self.KEY_MAX_LENGTH]
 
-		content = ["{:s}".format(name.center(16))]
+		it_len = len(str(nb_iterations))
+
+		start_len = 0
+		nb_prints = 0
+		if self.print_epoch:
+			start_len += len("Epoch EEE")
+			nb_prints += 1
+
+		if self.print_it:
+			start_len += len("It " + "I" * it_len + "/" + "I" * it_len)
+			nb_prints += 1
+
+		if self.print_progression_percent:
+			start_len += len("XXX%")
+			nb_prints += 1
+
+		start_len += len(" - ") * (nb_prints - 1)
+
+		content = ["{:s}".format(name.center(start_len))]
 		content += [filter_name(metric_name) for metric_name in keys]
 		content += ["took (s)".center(self.KEY_MAX_LENGTH)]
 
--- mlu/utils/printers/line.py
+++ mlu/utils/printers/line.py
@@ -7,6 +7,9 @@ from typing import Dict
 class LinePrinter(PrinterABC):
 	def __init__(
 		self,
+		print_epoch: bool = True,
+		print_it: bool = False,
+		print_progression_percent: bool = False,
 		print_exec_time: bool = True,
 	):
 		"""
@@ -16,13 +19,23 @@ class LinePrinter(PrinterABC):
 
 			>>> printer = LinePrinter()
 			>>> printer.print_current_values({"train/accuracy": 0.89, "train/loss": 1.525}, 33, 100, 2, "train")
-			... 'train, epoch   2,  33%, accuracy: 8.9000e-01, loss: 1.5250e+00, took (s): 0.0'
+			... 'train, epoch   2,  33%, accuracy: 8.9000e-01, loss: 1.5250e+00, took (s): 0.0, it: 33/100'
 
+			:param print_epoch: TODO
+				(default: True)
+			:param print_it: Print the iteration over the max number of iterations.
+				(default: False)
+			:param print_progression_percent: TODO
+				(default: False)
 			:param print_exec_time: Print time elapsed with the beginning of the loop (iteration == 0).
+				(default: True)
 		"""
 		self.print_exec_time = print_exec_time
+		self.print_epoch = print_epoch
+		self.print_it = print_it
+		self.print_progression_percent = print_progression_percent
 
-		self._epoch_start_date = time()
+		self._epoch_start_time = time()
 
 	def print_current_values(
 		self,
@@ -33,19 +46,34 @@ class LinePrinter(PrinterABC):
 		name: str,
 	):
 		if iteration == 0:
-			self._epoch_start_date = time()
+			self._epoch_start_time = time()
 
-		keys = list(current_values.keys())
 		if name is None:
+			keys = list(current_values.keys())
 			name = "/".join(keys[0].split("/")[:-1]) if "/" in keys[0] else ""
-		progression = int(100 * (iteration + 1) / nb_iterations)
-		current_means = {name.split("/")[-1]: value for name, value in current_values.items()}
 
+		content = ["{:5s}".format(name)]
+
+		if self.print_epoch:
+			content.append("epoch {:3d}".format(epoch))
+
+		if self.print_it:
+			it_format = f"{{:{len(str(nb_iterations))}d}}"
+			it_frac_format = f"it:{it_format}/{it_format}"
+			content.append(it_frac_format.format(iteration, nb_iterations))
+
+		if self.print_progression_percent:
+			progression = int(100 * (iteration + 1) / nb_iterations)
+			content.append("{:3d}%".format(progression))
+
+		current_means = {name.split("/")[-1]: value for name, value in current_values.items()}
 		content = ["{:s}: {:.4e}".format(name, value) for name, value in current_means.items()]
+
 		if self.print_exec_time:
-			content.append("{:s}: {:.2f}".format("took (s)", time() - self._epoch_start_date))
+			content.append("{:s}: {:.2f}".format("took (s)", time() - self._epoch_start_time))
+
 		content = ", ".join(content)
-		print("{:5s}, epoch {:3d}, {:3d}%, {:s}".format(name, epoch+1, progression, content), end="\r")
+		print(content, end="\r")
 
 		if iteration == nb_iterations - 1:
 			print("")
