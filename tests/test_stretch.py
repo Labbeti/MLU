4ac720e153bc212612903826e6211967627d9a02
Labbeti <etienne.labbe31@gmail.com>
2021-03-17 14:58:19 +0100

    Mod: Update test.

--- tests/test_stretch.py
+++ tests/test_stretch.py
@@ -1,16 +1,24 @@
 
+# import numpy as np
 import torch
 
-from mlu.transforms.waveform.stretch_pad_crop import StretchPadCrop
 from unittest import TestCase, main
+from mlu.transforms.waveform.stretch_pad_crop import StretchPadCrop
 
 
 class TestStretch(TestCase):
 	def test_stretch_pad_crop(self):
-		x = torch.rand(320000)  # ADS input example
+		# ADS input example
+		# note : transforms are not supposed to work with numpy arrays !
+		# x1 = np.random.rand(320000)
+		x2 = torch.rand(320000)
+
 		stretch = StretchPadCrop()
-		xa = stretch(x)
-		self.assertEqual(x.shape, xa.shape)
+
+		# xa = stretch(x1)
+		# self.assertEqual(x1.shape, xa.shape)
+		xa = stretch(x2)
+		self.assertEqual(x2.shape, xa.shape)
 
 
 if __name__ == "__main__":
