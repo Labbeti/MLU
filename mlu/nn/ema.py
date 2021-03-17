b20957d1b49de69f07cacbb3022579a6f7918638
Labbeti <etienne.labbe31@gmail.com>
2021-03-18 00:05:54 +0100

    Add: Forward on wrapped model for EMA.

--- mlu/nn/ema.py
+++ mlu/nn/ema.py
@@ -1,9 +1,11 @@
 
+import copy
+
 from torch.nn import Module
 
 
-class EMA:
-	def __init__(self, model: Module, decay: float = 0.99):
+class EMA(Module):
+	def __init__(self, model: Module, decay: float = 0.99, copy_model: bool = True):
 		"""
 			Compute the exponential moving average of a model.
 
@@ -11,8 +13,11 @@ class EMA:
 
 			:param model: The target model to update.
 			:param decay: The exponential decay (sometimes called "alpha") used to update the model. (default: 0.99)
+			:param copy_model: If true, the model passed as input will be copied. (default: True)
 		"""
 		super().__init__()
+		if copy_model:
+			model = copy.deepcopy(model)
 		self.model = model
 		self.decay = decay
 
@@ -24,6 +29,9 @@ class EMA:
 
 		for param, other_param in zip(model_params, other_params):
 			param.set_(self.decay * param + (1.0 - self.decay) * other_param)
+
+	def forward(self, *args, **kwargs):
+		return self.model(*args, **kwargs)
 
 	def set_model(self, model: Module):
 		self.model = model
