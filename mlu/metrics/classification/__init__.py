2c58f0a745038e60775e4204b92c782c72235c2e
Labbeti <etienne.labbe31@gmail.com>
2021-04-08 15:25:39 +0200

    Add: MSE to imports.

--- mlu/metrics/classification/__init__.py
+++ mlu/metrics/classification/__init__.py
@@ -5,6 +5,7 @@ from .accuracy import CategoricalAccuracy
 from .dprime import DPrime
 from .equal import Equal
 from .fscore import FScore
+from .mse import MSE
 from .precision import Precision
 from .recall import Recall
 from .rocauc import RocAuc
