b8e84037623255dc037ec93cb3da500e9ee1dbc9
Labbeti <etienne.labbe31@gmail.com>
2021-04-14 19:05:29 +0200

    Add: CutOutSpec with differents values across channels.

--- mlu/transforms/spectrogram/cutoutspec.py
+++ mlu/transforms/spectrogram/cutoutspec.py
@@ -1,9 +1,9 @@
 
-import logging
 import math
 import torch
 
 from torch import Tensor
+from torch.distributions import Uniform
 from typing import Tuple, Union
 
 from mlu.transforms.base import SpectrogramTransform
@@ -14,70 +14,101 @@ class CutOutSpec(SpectrogramTransform):
 		self,
 		freq_scales: Tuple[float, float] = (0.1, 0.5),
 		time_scales: Tuple[float, float] = (0.1, 0.5),
-		fill_value: float = -80.0,
-		p: float = 1.0,
+		fill_value: Union[float, Tuple[float, float]] = -100.0,
+		same_across_channels: bool = True,
 		freq_dim: int = -2,
 		time_dim: int = -1,
-		width_scales: Union[float, Tuple[float, float], None] = None,
-		height_scales: Union[float, Tuple[float, float], None] = None,
+		p: float = 1.0,
 	):
 		"""
 			CutOut transform for spectrogram tensors.
 
 			Input must be of shape (..., freq, time).
 
-			:param width_scales: The range of ratios of the rectangle cut width. (default: (0.1, 0.5))
-			:param height_scales: The range of ratios of the rectangle cut height. (default: (0.1, 0.5))
-			:param fill_value: The value used for fill. (default: -80.0)
-			:param p: The probability to apply the transform. (default: 1.0)
+			Example :
+
+			>>> from mlu.transforms import CutOutSpec
+			>>> spectrogram = ...
+			>>> augment = CutOutSpec((0.5, 0.5), (0.5, 0.5))
+			>>> # Cut 25% of the spectrogram values
+			>>> spectrogram_augmented = augment(spectrogram)
+
+			:param freq_scales: The range of ratios for the frequencies dim. (default: (0.1, 0.5))
+			:param time_scales: The range of ratios for the time steps dim. (default: (0.1, 0.5))
+			:param fill_value: The value used for fill. (default: -100.0)
 			:param freq_dim: The dimension index of the spectrogram frequencies (default: -2)
 			:param time_dim: The dimension index of the spectrogram time steps (default: -1)
+			:param p: The probability to apply the transform. (default: 1.0)
 		"""
+		if freq_dim == time_dim:
+			raise ValueError('Frequency dimension index cannot be the same than time dimension index.')
+
 		super().__init__(p=p)
-
-		if width_scales is not None:
-			logging.warning(
-				'(package MLU): Parameter name "width_scales" is depreciated. Please use "freq_scales" instead.'
-			)
-			freq_scales = width_scales if isinstance(width_scales, tuple) else (width_scales, width_scales)
-
-		if height_scales is not None:
-			logging.warning(
-				'(package MLU): Parameter name "height_scales" is depreciated. Please use "time_scales" instead.'
-			)
-			time_scales = height_scales if isinstance(height_scales, tuple) else (height_scales, height_scales)
 
 		self.freq_scales = freq_scales
 		self.time_scales = time_scales
 		self.fill_value = fill_value
+		self.same_across_channels = same_across_channels
 		self.freq_dim = freq_dim
 		self.time_dim = time_dim
 
 	def process(self, data: Tensor) -> Tensor:
-		if len(data.shape) < 2:
+		if not isinstance(data, Tensor) or len(data.shape) < 2:
 			raise RuntimeError(
-				f'Invalid data shape {data.shape}.'
-				f'Must have at least 2 dimensions but found {len(data.shape)}.'
+				f'Input data must be a pytorch Tensor with at least 2 dimensions for CutOutSpec transform, '
+				f'found {type(data)}' + (f' of shape {data.shape}' if hasattr(data, 'shape') else '') + '.'
 			)
 
+		data = data.clone()
+
+		if self.same_across_channels:
+			data = self._process_same_across_channels(data)
+		else:
+			n_dims = len(data.shape)
+			if n_dims != 3 or self.freq_dim != -2 or self.time_dim != -1:
+				raise NotImplementedError(
+					f'When same_across_channels=False, n_dims must be equal to 3, freq_dim to -2 and time_dim to -1. '
+					f'(n_dims={n_dims}, freq_dim={self.freq_dim}, time_dim={self.time_dim})'
+				)
+
+			batch_dim = 0
+			indexes: list = [slice(None) for _ in range(n_dims)]
+			for i in range(data.shape[batch_dim]):
+				indexes[batch_dim] = i
+				data[indexes] = self._process_same_across_channels(data[indexes])
+
+		return data
+
+	def _process_same_across_channels(self, data: Tensor) -> Tensor:
+		# Prepare slices indexes for frequencies and time dimensions
 		slices = [slice(None)] * len(data.shape)
 
-		# Prepare slices indexes for frequencies and time dimensions
-		slices[self.freq_dim] = self._gen_slice(data.shape[self.freq_dim], self.freq_scales)
-		slices[self.time_dim] = self._gen_slice(data.shape[self.time_dim], self.time_scales)
+		freq_start, freq_end, freq_size = self._gen_range(data.shape[self.freq_dim], self.freq_scales)
+		time_start, time_end, time_size = self._gen_range(data.shape[self.time_dim], self.time_scales)
+		slices[self.freq_dim] = slice(freq_start, freq_end)
+		slices[self.time_dim] = slice(time_start, time_end)
+
+		if isinstance(self.fill_value, tuple):
+			uniform = Uniform(low=self.fill_value[0], high=self.fill_value[1])
+			shape = torch.ones(len(data.shape), dtype=torch.int)
+			shape[self.freq_dim] = freq_size
+			shape[self.time_dim] = time_size
+			fill_value = uniform.sample(shape.tolist())
+		else:
+			fill_value = self.fill_value
 
 		# Set are to fill_value
-		data = data.clone()
-		data[slices] = self.fill_value
+		data[slices] = fill_value
 		return data
 
 	@staticmethod
-	def _gen_slice(size: int, scales: Tuple[float, float]) -> slice:
+	def _gen_range(size: int, scales: Tuple[float, float]) -> Tuple[int, int, int]:
 		cutout_size_min = math.ceil(scales[0] * size)
 		cutout_size_max = max(math.ceil(scales[1] * size), cutout_size_min + 1)
 		cutout_size = torch.randint(cutout_size_min, cutout_size_max, ())
 
-		cutout_start = torch.randint(0, max(size - cutout_size, 1), ())
-		cutout_end = max(cutout_start + cutout_size, cutout_start + 1)
+		cutout_start = torch.randint(0, max(size - cutout_size + 1, 1), ())
+		cutout_end = cutout_start + cutout_size
+		assert cutout_end - cutout_start == cutout_size, f'{cutout_end} - {cutout_start} != {cutout_size}'
 
-		return slice(cutout_start, cutout_end)
+		return cutout_start, cutout_end, cutout_size
--- tests/transforms/test_cutoutspec.py
+++ tests/transforms/test_cutoutspec.py
@@ -7,14 +7,6 @@ from mlu.transforms import CutOutSpec
 
 
 class TestCutOutSpec(TestCase):
-	def test_cos_2(self):
-		cutoutspec = CutOutSpec(fill_value=0.0, freq_scales=(0.4, 0.4), time_scales=(0.6, 0.6))
-		spec = torch.ones(10, 100)
-		spec_cut = cutoutspec(spec)
-		self.assertFalse(spec.eq(spec_cut).all())
-		self.assertGreater(spec.sum(), spec_cut.sum())
-		self.assertEqual(spec_cut.eq(0.0).sum(), 10 * 0.4 * 100 * 0.6)
-
 	def test_cos(self):
 		cutoutspec = CutOutSpec(fill_value=0.0)
 
@@ -23,6 +15,63 @@ class TestCutOutSpec(TestCase):
 
 		# print(spec)
 		# print(spec_cut)
+		self.assertFalse(spec.eq(spec_cut).all())
+
+	def test_cos_2(self):
+		cutoutspec = CutOutSpec(fill_value=0.0, freq_scales=(0.4, 0.4), time_scales=(0.6, 0.6))
+		spec = torch.ones(10, 100)
+		spec_cut = cutoutspec(spec)
+		self.assertFalse(spec.eq(spec_cut).all())
+		self.assertGreater(spec.sum(), spec_cut.sum())
+		self.assertEqual(spec_cut.eq(0.0).sum(), 10 * 0.4 * 100 * 0.6)
+
+	def test_cos_3(self):
+		spec = torch.ones(10, 100)
+		scales = [0.0, 0.5, 1.0]
+
+		for f_scale in scales:
+			for t_scale in scales:
+				cutoutspec = CutOutSpec(fill_value=0.0, freq_scales=(f_scale, f_scale), time_scales=(t_scale, t_scale))
+				spec_cut = cutoutspec(spec)
+				self.assertEqual(
+					spec_cut.eq(0.0).sum(), 10 * f_scale * 100 * t_scale, f"Assert errors for scale {f_scale} and {t_scale}.")
+
+	def test_fill_values(self):
+		spec = torch.stack([
+			torch.full((2, 5), 1.0),
+			torch.full((2, 5), 2.0),
+			torch.full((2, 5), 3.0),
+		])
+		cutoutspec = CutOutSpec(
+			fill_value=(0.0, 1.0),
+			freq_scales=(0.5, 0.5),
+			time_scales=(0.5, 0.5),
+			freq_dim=1,
+			time_dim=2,
+		)
+		spec_cut = cutoutspec(spec)
+
+		# print('Shape: ', spec.shape)
+		# print(spec_cut)
+		self.assertFalse(spec.eq(spec_cut).all())
+
+	def test_not_same_across_channels(self):
+		spec = torch.stack([
+			torch.full((4, 6), 1.0),
+			torch.full((4, 6), 2.0),
+			torch.full((4, 6), 3.0),
+		])
+		cutoutspec = CutOutSpec(
+			fill_value=(0.0, 0.0),
+			freq_scales=(0.25, 0.25),
+			time_scales=(0.5, 0.5),
+			freq_dim=-2,
+			time_dim=-1,
+			same_across_channels=False,
+		)
+		spec_cut = cutoutspec(spec)
+		print('Shape: ', spec.shape)
+		print(spec_cut)
 		self.assertFalse(spec.eq(spec_cut).all())
 
 
